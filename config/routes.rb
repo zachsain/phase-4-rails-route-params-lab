9e7abfc49c7df2f969844e94a62c2b58a11340e0
Zachary Sain <zachsain@gmail.com>
2022-05-17 14:09:16 -0500

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+
+  get 'students/:id', to: "students#show"
+
 end
